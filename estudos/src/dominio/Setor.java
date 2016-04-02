f0e12591274fbd139e072fb7b4335fce58bc80c4
Istanislau <edsonistanislau@live.com>
2016-04-02 20:01:21 -0300

    criação do primeiro mapeamento

---
+++ estudos/src/dominio/Setor.java
@@ -0,0 +1,66 @@
+package dominio;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "setor")
+public class Setor implements Serializable{
+	
+	@Id					//Informa que é chave primaria
+	@GeneratedValue		//informa que é uma sequencia gerada pelo banco
+	private Integer id;
+	
+	private String nome;
+	
+	private Integer ramal;
+	
+	private Boolean flgAtivo;
+	
+	private Date dtCadastro;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getRamal() {
+		return ramal;
+	}
+
+	public void setRamal(Integer ramal) {
+		this.ramal = ramal;
+	}
+
+	public Boolean getFlgAtivo() {
+		return flgAtivo;
+	}
+
+	public void setFlgAtivo(Boolean flgAtivo) {
+		this.flgAtivo = flgAtivo;
+	}
+
+	public Date getDtCadastro() {
+		return dtCadastro;
+	}
+
+	public void setDtCadastro(Date dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+}
